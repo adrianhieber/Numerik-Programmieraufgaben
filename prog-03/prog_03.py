d12e7e862df9454479172a500b7d83348e04d875
adrianhieber <adrian.hieber@gmail.com>
2022-12-05 16:55:43 +0100

    blacked

--- prog-03/prog_03.py
+++ prog-03/prog_03.py
@@ -2,14 +2,12 @@ import numpy as np
 import substitution_msl
 import matplotlib.pyplot as plt
 
-#just for test, delete after
+# just for test, delete after
 def isNaN(num):
     return num != num
 
-def qr(A, mode="full", alg="Householder"):
-    # check alg
-    if alg != "Householder":
-        raise Exception("ich hab nur den algorithmus, also benutz ihn")
+
+def qr_householder(A, mode):
 
     # check for Matrix A
     m = A.shape[0]
@@ -32,7 +30,7 @@ def qr(A, mode="full", alg="Householder"):
             print(vs)
             print(np.linalg.norm(vs))
             raise Exception("tf")
-        
+
         vs = vs / np.linalg.norm(vs)
 
         Qs = np.eye(m - j, dtype=float) - 2 * np.array([vs]).T @ np.array([vs])
@@ -50,29 +48,44 @@ def qr(A, mode="full", alg="Householder"):
         return R[:n, :n]
 
 
-def aii(m,n):
-    interval=[-3,3]
-    
-    xi = np.linspace(interval[0],interval[1],m)
-    epsi = np.random.normal(0,0.05,m)
-    yi = [np.sin(3*xi[i])+xi[i]+epsi[i] for i in range(m)]
+def qr_givens(A, mode):
+    pass
 
-    V=np.vander(xi,n)
-    Q,R=qr(V,mode="reduced")
-    
-    p=substitution_msl.backwardSubstitution(R,Q.T@yi)
-    
+
+def qr(A, mode="full", alg="Householder"):
+    if alg == "Householder":
+        return qr_householder(A, mode)
+    if alg == "Givens":
+        return qr_givens(A, mode)
+
+    raise Exception("Unknwon algortihm")
+
+
+def aii(m, n):
+    interval = [-3, 3]
+
+    xi = np.linspace(interval[0], interval[1], m)
+    epsi = np.random.normal(0, 0.05, m)
+    yi = [np.sin(3 * xi[i]) + xi[i] + epsi[i] for i in range(m)]
+
+    V = np.vander(xi, n)
+    Q, R = qr(V, mode="reduced")
+
+    p = substitution_msl.backwardSubstitution(R, Q.T @ yi)
+
     return xi, p
+
 
 def my_plot():
     fig = plt.figure()
-    #for i in range(5,10):
-        #n=2**i
-    xi,p=aii(100,50)
-    plt.plot(xi,p,label=50)
+    # for i in range(5,10):
+    # n=2**i
+    xi, p = aii(100, 50)
+    plt.plot(xi, p, label=50)
     ###hä irgendwie machts ja kein sinn xi mit m elementen und p mit n-1 elementen zu plotten
     plt.legend()
     plt.show()
+
 
 if __name__ == "__main__":
     my_plot()
--- prog-03/substitution_msl.py
+++ prog-03/substitution_msl.py
@@ -1,30 +1,32 @@
 import numpy as np
 
+
 def check_input(A, b, criterions=[], tol=1e-12):
     # Überprüfe ob A zweidimensional
-    if ('2-dim' in criterions) and (np.ndim(A) != 2):
-        raise(ValueError("Die Matrix ist nicht zwei-dimensional!"))
+    if ("2-dim" in criterions) and (np.ndim(A) != 2):
+        raise (ValueError("Die Matrix ist nicht zwei-dimensional!"))
 
     # Überprüfe ob A quadratisch
-    if ('quadratic' in criterions) and (A.shape[0] != A.shape[1]): 
-        raise(ValueError("Die Matrix ist nicht quadratisch."))
-        
+    if ("quadratic" in criterions) and (A.shape[0] != A.shape[1]):
+        raise (ValueError("Die Matrix ist nicht quadratisch."))
+
     # Überprüfe ob A obere Dreiecksmatirx
-    if ('triangular' in criterions) and np.any(np.tril(A, -1)): 
-        raise(ValueError("Die Matrix ist keine obere Diagonalmatrix."))
-        
+    if ("triangular" in criterions) and np.any(np.tril(A, -1)):
+        raise (ValueError("Die Matrix ist keine obere Diagonalmatrix."))
+
     # Überprüfe ob A nullen auf der Diagonale hat
-    if ('zero-diag' in criterions) and (np.min(np.abs(np.diag(A)))<tol):
-        raise(ValueError("Die Matrix hat Nullen auf der Diagonale."))
+    if ("zero-diag" in criterions) and (np.min(np.abs(np.diag(A))) < tol):
+        raise (ValueError("Die Matrix hat Nullen auf der Diagonale."))
 
     # Überprüfe, dass b im richtigen Format ist
     if np.ndim(b) != 1:
-        raise(ValueError("Der Vektor soll Shape (n,) haben!"))
-        
+        raise (ValueError("Der Vektor soll Shape (n,) haben!"))
+
     # Überprüfe, dass und b kompatibel sind.
     if b.shape[0] != A.shape[1]:
-        raise(ValueError("Matrix und Vektor sind nicht kompatibel!"))
-    
+        raise (ValueError("Matrix und Vektor sind nicht kompatibel!"))
+
+
 def backwardSubstitution(R, b):
     """backwardSubstitution
     ========
