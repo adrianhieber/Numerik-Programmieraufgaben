48a91568701df6c1e6840f8da6108ff5f855bae9
adrianhieber <adrian.hieber@gmail.com>
2022-12-07 09:25:50 +0100

    givens first attemt

--- prog-03/prog_03.py
+++ prog-03/prog_03.py
@@ -1,6 +1,7 @@
 import numpy as np
 import substitution_msl
 import matplotlib.pyplot as plt
+from math import copysign, hypot
 
 
 def qr_householder(A, mode):
@@ -40,15 +41,74 @@ def qr_householder(A, mode):
 def qr_givens(A, mode):
     pass
 
+def qr_givens_online(A,mode):
+    m, n = A.shape
+    Q = np.eye(m)  # Initialize Q as the identity matrix
+    R = A.copy()   # Initialize R as a copy of A
+    
+    # Iterate over the columns of R
+    for i in range(n):
+        # Iterate over the rows of R, starting from the i-th row
+        for j in range(i+1, m):
+            # Compute the Givens rotation matrix that zeros out the (j, i) element of R
+            G = np.eye(m)
+            r = np.sqrt(R[i, i]**2 + R[j, i]**2)
+            c = R[i, i] / r
+            s = -R[j, i] / r
+            G[[i, j], [i, j]] = c
+            G[i, j] = s
+            G[j, i] = -s
+            
+            # Apply the Givens rotation to R and Q
+            R = np.dot(G, R)
+            Q = np.dot(Q, G.T)
+            
+    return Q[:m, :n], R[:n, :n]
 
-def qr(A, mode="full", alg="Householder"):
+def _givens_rotation_matrix_entries(a, b):
+    """Compute matrix entries for Givens rotation."""
+    r = hypot(a, b)
+    c = a/r
+    s = -b/r
+
+    return (c, s)
+
+def givens_rotation(A,mode):
+    """Perform QR decomposition of matrix A using Givens rotation."""
+    (num_rows, num_cols) = np.shape(A)
+
+    # Initialize orthogonal matrix Q and upper triangular matrix R.
+    Q = np.identity(num_rows)
+    R = np.copy(A)
+
+    # Iterate over lower triangular matrix.
+    (rows, cols) = np.tril_indices(num_rows, -1, num_cols)
+    for (row, col) in zip(rows, cols):
+
+        # Compute Givens rotation matrix and
+        # zero-out lower triangular matrix entries.
+        if R[row, col] != 0:
+            (c, s) = _givens_rotation_matrix_entries(R[col, col], R[row, col])
+
+            G = np.identity(num_rows)
+            G[[col, row], [col, row]] = c
+            G[row, col] = s
+            G[col, row] = -s
+
+            R = np.dot(G, R)
+            Q = np.dot(Q, G.T)
+
+    return Q[:num_rows, :num_cols], R[:num_cols, :num_cols]
+
+
+def qr(A, mode="full", alg="givens_rotation"):
     if not isinstance(A, np.ndarray):
         raise Exception("usage: qr_householder(A, mode, alg), A has to be numpy array")
 
     if alg == "Householder":
         return qr_householder(A, mode)
-    if alg == "Givens":
-        return qr_givens(A, mode)
+    if alg == "givens_rotation":
+        return givens_rotation(A, mode)
 
     raise Exception("Unknwon algortihm")
 
