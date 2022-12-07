202b794587d6a6641f87538a509503f1c01b2aab
adrianhieber <adrian.hieber@gmail.com>
2022-12-07 10:18:13 +0100

    givens finished

--- prog-03/prog_03.py
+++ prog-03/prog_03.py
@@ -1,24 +1,19 @@
 import numpy as np
 import substitution_msl
 import matplotlib.pyplot as plt
-from math import copysign, hypot
 
 
 def qr_householder(A, mode):
 
-    # check for Matrix A
+    #init variables
     m = A.shape[0]
     n = A.shape[1]
-
-    if m < n:
-        raise Exception("for matrix A=Mat(mxn), m>=n")
-
-    # calculate
     Q = np.eye(m, dtype=float)
     R = np.array(A.copy(), dtype=float)
-
+    
+    # calculate
     for j in range(0, n):
-        vs = R.copy()[j:m, j]  # warum muss hier ein copy hin??
+        vs = R.copy()[j:m, j]
         vs[0] = vs[0] - np.linalg.norm(vs)
 
         vs = vs / np.linalg.norm(vs)
@@ -39,58 +34,60 @@ def qr_householder(A, mode):
 
 
 def qr_givens(A, mode):
-    pass
+
+    #init variables
+    m = A.shape[0]
+    n = A.shape[1]
+    Q = np.eye(m, dtype=float)
+    R = np.array(A.copy(), dtype=float)
+
+    #calculate
+    for i in range(1, m):
+        for j in range(0, n):
+
+            a1 = R[j, j]
+            a2 = R[i, j]
+            anorm = np.linalg.norm(np.array([a1, a2]))
+            c = a1 / anorm
+            s = -a2 / anorm
+
+            Qij = np.eye(m, dtype=float)
+            Qij[i, i] = c
+            Qij[j, j] = c
+            Qij[i, j] = s
+            Qij[j, i] = -s
+
+            R = np.dot(Qij, R)
+            if mode != "R":
+                Q = np.dot(Q, Qij.T)
+                
+    # return based on mode
+    if mode == "full":
+        return Q, R
+    if mode == "reduced":
+        return Q[:m, :n], R[:n, :n]
+    if mode == "R":
+        return R[:n, :n]
 
 
-def _givens_rotation_matrix_entries(a, b):
-    """Compute matrix entries for Givens rotation."""
-    r = hypot(a, b)
-    c = a/r
-    s = -b/r
-
-    return (c, s)
-
-def givens_rotation(A,mode):
-    """Perform QR decomposition of matrix A using Givens rotation."""
-    (num_rows, num_cols) = np.shape(A)
-
-    # Initialize orthogonal matrix Q and upper triangular matrix R.
-    Q = np.identity(num_rows)
-    R = np.copy(A)
-
-    # Iterate over lower triangular matrix.
-    (rows, cols) = np.tril_indices(num_rows, -1, num_cols)
-    for (row, col) in zip(rows, cols):
-
-        # Compute Givens rotation matrix and
-        # zero-out lower triangular matrix entries.
-        if R[row, col] != 0:
-            (c, s) = _givens_rotation_matrix_entries(R[col, col], R[row, col])
-
-            G = np.identity(num_rows)
-            G[[col, row], [col, row]] = c
-            G[row, col] = s
-            G[col, row] = -s
-
-            R = np.dot(G, R)
-            Q = np.dot(Q, G.T)
-
-    return Q[:num_rows, :num_cols], R[:num_cols, :num_cols]
-
-
-def qr(A, mode="full", alg="givens_rotation"):
+def qr(A, mode="full", alg="Householder"):
+    
     if not isinstance(A, np.ndarray):
         raise Exception("usage: qr_householder(A, mode, alg), A has to be numpy array")
 
+    if A.shape[0] < A.shape[1]:
+        raise Exception("for matrix A=Mat(mxn), m>=n")
+
     if alg == "Householder":
         return qr_householder(A, mode)
-    if alg == "givens_rotation":
-        return givens_rotation(A, mode)
+    
+    if alg == "Givens":
+        return qr_givens(A, mode)
 
     raise Exception("Unknwon algortihm")
 
 
-def function_values(m, n):
+def function_values(m, n, alg):
     interval = [-3, 3]
 
     xi = np.linspace(interval[0], interval[1], m)
@@ -98,7 +95,7 @@ def function_values(m, n):
     yi = [np.sin(3 * xi[i]) + xi[i] + epsi[i] for i in range(m)]
 
     V = np.vander(xi, n)
-    Q, R = qr(V, mode="reduced")
+    Q, R = qr(V, mode="reduced",alg=alg)
 
     p = substitution_msl.backwardSubstitution(R, Q.T @ yi)
 
@@ -112,13 +109,13 @@ def reversed_horner(p, x):
     return y
 
 
-def my_plot():
+def my_plot(alg):
     fig = plt.figure()
     realy_plotted = False
     m = 50
 
     for n in [5, 9, 20]:
-        xi, realy, p = function_values(m, n)
+        xi, realy, p = function_values(m, n,alg)
 
         if not realy_plotted:
             plt.plot(xi, realy, ".", label="y")
@@ -127,10 +124,11 @@ def my_plot():
         ylist = [reversed_horner(p, x) for x in xi]
         plt.plot(xi, ylist, label=f"n={n}")
 
-    plt.title(f"Comparison with m={m}")
+    plt.title(f"Comparison with m={m}, Alg={alg}")
     plt.legend()
     plt.show()
 
 
 if __name__ == "__main__":
-    my_plot()
+    #a1 visualize:
+    my_plot(alg="Householder")
